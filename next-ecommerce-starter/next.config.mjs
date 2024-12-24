acc81065f8a8385629f6f90f330db0a07a43661f
Ravin Yadav <yravin72@gmail.com>
2024-12-24 13:57:21 +0530

    e-commerce website making frontend and connecting to wix studio

---
+++ next-ecommerce-starter/next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['images.pexels.com','static.wixstatic.com'], // Add this domain
+        
+      },
+};
+
+export default nextConfig;
